e9e3caa8b3eacd1697e7b28971468fc8fd5230a4
Iain <iain@zora.co>
2021-12-02 10:57:24 -0500

    bid modal cleanup

---
+++ src/utils/formatAmount.tsx
@@ -0,0 +1,16 @@
+import { formatEther } from "@ethersproject/units";
+import { BigNumberish } from "ethers";
+import React from "react";
+import { useThemeConfig } from "../hooks/useThemeConfig";
+
+export const formatAmount = (text: string, amount: BigNumberish) => {
+  const { getStyles } = useThemeConfig();
+  const formattedAmount = formatEther(amount).substr(0, 5);
+  const textSplit = text.split('%');
+  return textSplit.map((part, i) => {
+    if (i === textSplit.length - 1) {
+      return <React.Fragment>{part}</React.Fragment>;
+    }
+    return <span>{part} <span {...getStyles('ethAmount')}>{formattedAmount} eth</span></span>;
+  })
+};
